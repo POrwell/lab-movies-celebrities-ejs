09e0834dd97659e75549f56d0101065d0cd46000
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-31 16:35:03 +0100

    Iteration2 complete

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,20 @@
-//  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebSchema = new Schema ({
+name: {
+    type: String,
+    required: true
+},
+occupation: {
+    type: String,
+    required: true
+},
+catchPhrase: {
+    type: String,
+    required: true 
+}
+})
+
+const Celeb = mongoose.model("Celeb", celebSchema);
+module.exports = Celeb;
