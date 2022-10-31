3c4bb1fe19953041e8253aa057233c7b45f4b557
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-31 17:24:41 +0100

    Iteration3 complete

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,22 @@
 const router = require('express').Router()
+const Celeb = require("../models/Celebrity.model")
 
 // all your routes here
+router.get("/celebrities/create", (req, res) => {
+    res.render("celebrities/new-celebrity", { celebrity: {name: "", occupation: "", catchPhrase: ""} })
+})
+
+router.post("/celebrities/create", async (req, res) => { 
+    try {
+    await Celeb.create({
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+    })
+    res.redirect("/celebrities")
+} catch (error) {
+    res.render("celebrities/new-celebrity")
+}
+})
 
 module.exports = router
